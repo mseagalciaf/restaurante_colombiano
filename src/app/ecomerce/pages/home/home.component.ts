d364b533d7135c72334f6717ddabe94287b53b71
Michael <ms.moncada22@ciaf.edu.co>
2021-05-17 17:15:15 -0500

    se muestra categories de la api en el sidebar categories

--- src/app/ecomerce/pages/home/home.component.ts
+++ src/app/ecomerce/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from 'src/app/config/config.service';
+import { CategoryInterface } from 'src/app/interfaces/category-inteface';
+import { CategoryService } from '../store/services/category.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  url_images:string = ConfigService.URL_IMAGES;
+  categories:CategoryInterface[];
+  constructor(
+    private categoryService : CategoryService
+  ) { }
 
   ngOnInit(): void {
+    this.getAllCategories();
+  }
+
+  getAllCategories(){
+    this.categoryService.getAllCategories().subscribe(
+      resp => {
+        this.categories = resp.data;
+        this.searchImage();
+      },
+      error => {
+        console.log(error)        
+      }
+    )
+  }
+
+  searchImage(){
+    this.categories = this.categories.map((category) => {
+      return {
+        ...category,
+        image: category.image
+          ? `${this.url_images}${category.image}`
+          : 'assets/icons/icons-categories/default.jpg',
+      };
+    });    
   }
 
 }
