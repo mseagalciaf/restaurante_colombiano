bac1fef7a234279b6d90a40ea7b60f5386bf6e9d
Michael <ms.moncada22@ciaf.edu.co>
2021-05-17 19:17:03 -0500

    las bubbles ya muestran los datos de la api

--- src/app/ecomerce/pages/store/components/bubble-categories/bubble-categories.component.ts
+++ src/app/ecomerce/pages/store/components/bubble-categories/bubble-categories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from 'src/app/config/config.service';
 import { CategoryInterface } from 'src/app/interfaces/category-inteface';
 import { CategoryService } from '../../services/category.service';
 
@@ -9,6 +10,7 @@ import { CategoryService } from '../../services/category.service';
 })
 export class BubbleCategoriesComponent implements OnInit {
 
+  url_images:string = ConfigService.URL_IMAGES;
   categories: CategoryInterface[];
 
   constructor(
@@ -22,11 +24,24 @@ export class BubbleCategoriesComponent implements OnInit {
   getCategories(){
     this.categoryService.getAllCategories().subscribe(
       resp => {
-        this.categories = resp.data
+        this.categories = resp.data;
+        this.searchImage();
       },
       error => console.log(error)
       
     )
+  }
+
+  searchImage(){
+    this.categories = this.categories.map((category) => {
+      return {
+        ...category,
+        image: category.image
+          ? `${this.url_images}${category.image}`
+          : 'assets/icons/icons-categories/default.jpg',
+      };
+    });
+    
   }
 
 }
